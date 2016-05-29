481b6561f0832f2dc2689795f033b4da2149e8f2
Jan Rabe <cloudgazer3d@gmail.com>
2016-05-29 13:21:02 +0200

    added animations for click events in stackview

--- app/src/main/java/net/kibotu/berlinplaces/ui/places/PlacesStackFragment.java
+++ app/src/main/java/net/kibotu/berlinplaces/ui/places/PlacesStackFragment.java
@@ -2,8 +2,10 @@ package net.kibotu.berlinplaces.ui.places;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.view.View;
 
 import com.common.android.utils.logging.Logger;
+import com.dtx12.android_animations_actions.actions.Interpolations;
 
 import net.kibotu.berlinplaces.R;
 import net.kibotu.berlinplaces.ui.BaseFragment;
@@ -13,6 +15,9 @@ import link.fls.swipestack.SwipeStack;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
+import static com.dtx12.android_animations_actions.actions.Actions.play;
+import static com.dtx12.android_animations_actions.actions.Actions.scaleTo;
+import static com.dtx12.android_animations_actions.actions.Actions.sequence;
 import static net.kibotu.berlinplaces.network.RequestProvider.getLocations;
 
 /**
@@ -24,11 +29,29 @@ public class PlacesStackFragment extends BaseFragment {
     @BindView(R.id.swipeStack)
     SwipeStack swipeStack;
 
+    @BindView(R.id.undo)
+    View undo;
+    @BindView(R.id.reject)
+    View reject;
+    @BindView(R.id.like)
+    View like;
+    @BindView(R.id.favorite)
+    View favorite;
+
     @Override
     protected void onViewCreated(Bundle savedInstanceState) {
-        Logger.v(tag(), "onViewCreated ");
 
         downloadNearby();
+
+        undo.setOnClickListener(createZoomAnimationClickListener());
+        reject.setOnClickListener(createZoomAnimationClickListener());
+        like.setOnClickListener(createZoomAnimationClickListener());
+        favorite.setOnClickListener(createZoomAnimationClickListener());
+    }
+
+    @NonNull
+    private View.OnClickListener createZoomAnimationClickListener() {
+        return v -> play(sequence(scaleTo(1.75f, 1.75f, 0.25f, Interpolations.BackEaseIn), scaleTo(1f, 1f, 0.15f, Interpolations.BackEaseOut)), v);
     }
 
     @Override
