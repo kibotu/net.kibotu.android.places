b6f622d1c57c3b48f55b68666ec202c3c9d784f9
Jan Rabe <cloudgazer3d@gmail.com>
2016-05-29 15:32:04 +0200

    removed accidental app index commit

--- app/src/main/java/net/kibotu/berlinplaces/MainActivity.java
+++ app/src/main/java/net/kibotu/berlinplaces/MainActivity.java
@@ -1,7 +1,6 @@
 package net.kibotu.berlinplaces;
 
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,9 +9,6 @@ import android.view.View;
 
 import com.common.android.utils.ContextHelper;
 import com.common.android.utils.extensions.ResourceExtensions;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.ramotion.paperonboarding.PaperOnboardingFragment;
 import com.ramotion.paperonboarding.PaperOnboardingPage;
@@ -63,9 +59,6 @@ public class MainActivity extends BaseActivity implements DrawerManagerProvider 
 //                }, Throwable::printStackTrace);
 
         boarding();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void boarding() {
@@ -147,42 +140,6 @@ public class MainActivity extends BaseActivity implements DrawerManagerProvider 
     @Override
     public DrawerManager getDrawerManager() {
         return drawerManager;
-    }
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
     }
 }
 
